be944bfa528c1d73d7cadf11a2f3add80af7f4d2
HatanakaSuguru <suguru.hatanaka@jp.ricoh.com>
2025-09-02 18:01:09 +0900

    fix: update jest configuration and tsconfig settings; add test file

--- jest.config.ts
+++ jest.config.ts
@@ -1,8 +1,16 @@
-import { JestConfigWithTsJest } from 'ts-jest'
-
-// NOTE: jest.config.ts を TypeScript で記述するためには ts-node が必要
 export default {
-  preset: 'ts-jest/presets/js-with-ts-esm',
-  // see default value: https://jestjs.io/docs/configuration#testmatch-arraystring
-  testMatch: ['**/?(*.)+(test).(cjs|[jt]s?(x))'],
-} satisfies JestConfigWithTsJest
+  preset: 'ts-jest/presets/default-esm',
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.tsx?$': [
+      'ts-jest',
+      {
+        useESM: true,
+      },
+    ],
+  },
+  moduleNameMapper: {
+    '^(\\.{1,2}/.*)\\.ts$': '$1.ts',
+  },
+  extensionsToTreatAsEsm: ['.ts'],
+};
